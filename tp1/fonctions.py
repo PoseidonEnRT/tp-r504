c293930ff98d295c3ef66adaecc3769fab8ec56b
Kylian Laloux <lalouxkylian85@gmail.com>
2024-09-09 17:03:14 +0200

    prog

---
+++ tp1/fonctions.py
@@ -0,0 +1,4 @@
+def puissance(a,b):
+    return(a**b)
+    if not type(var) is int:
+        raise TypeError("Only integers are allowed")
--- tp1/prog1.py
+++ tp1/prog1.py
@@ -1,1 +1,8 @@
+import fonctions as f
+
 print("Hello, World!")
+while True:
+    a = input("Choisir nombre 1: ")
+    b = input("Choisir nombre 2: ")
+    res = f.puissance(int(a), int(b))
+    print(res)
