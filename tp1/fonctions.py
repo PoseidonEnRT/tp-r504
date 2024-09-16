3b97d47da36b780c7139ba9234d5e051fb635aa4
Kylian Laloux <lalouxkylian85@gmail.com>
2024-09-16 16:27:19 +0200

    MaJ test & fonctions .py

--- tp1/fonctions.py
+++ tp1/fonctions.py
@@ -1,4 +1,11 @@
-def puissance(a,b):
-    return(a**b)
+def puissance(a, b):
+    if a == 0 and b > 0:
+        return 1
+    elif b == 0:
+        print("On ne peut pas diviser par 0")
+    elif a == 0:
+        print("On ne peut pas mettre 0 à la puissance avec un négatif")
+    else:
+        return(a**b)
     if not type(var) is int:
-        raise TypeError("Only integers are allowed")
+        raise TypeError("Only intergers are allowed")
