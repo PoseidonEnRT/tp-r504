c5d1f14dd2a17f44d4ec7745d7a20a717291446c
PoseidonEnRT <160739009+PoseidonEnRT@users.noreply.github.com>
2025-04-30 15:11:42 +0200

    Update producer.sh
    aa

--- redis/test-local-1/producer.sh
+++ redis/test-local-1/producer.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+n=500
 redis-cli DBSIZE >/dev/null
 if ! [ $? = 0 ]
 then
   echo "Erreur, pas de connexion avec le serveur redis"
   exit 1
+  else
+  	echo "OK!"
 fi
 
-QUEUE_NAME="data_queue"
+
 while :
 do
-   for ((i=0;i<n;i++))
-   do
-     redis-cli LPUSH mafile $RANDOM
- done
-   sleep 3
+	taille=$(redis-cli LLEN mafile)
+	echo "taille file=$taille"
+	for ((i=0;i<$n;i++))
+	do
+		redis-cli LPUSH mafile $RANDOM >/dev/null
+	done
+	sleep 3
+done
+
  
